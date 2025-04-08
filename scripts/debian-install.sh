11384235d54f5729b51022406d4178583fa8b6e7
Subham <165278761+subhk02@users.noreply.github.com>
2025-04-08 13:34:13 +0530

    Update debian-install.sh

--- scripts/debian-install.sh
+++ scripts/debian-install.sh
@@ -3,7 +3,7 @@ if [[ $EUID -ne 0 ]]; then
     echo "Requesting sudo access to install packages..."
     sudo -v || exit 1
 fi
-sudo apt install --noconfirm python3 python3-venv python3-pip
+sudo apt install -y python3 python3-venv python3-pip
 
 cd ~/Documents
 git clone https://github.com/subhk02/FlappyBird-Game.git
@@ -11,5 +11,5 @@ cd FlappyBird-Game
 python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
-chmod +x flappy-bird.sh
-./flappy-bird.sh
+chmod +x dflappy-bird.sh
+./dflappy-bird.sh
